c24e4a2881edf88f45defad0c62cf1f1dc457243
developer <gownyferfff@gmail.com>
2022-05-14 10:22:37 +0300

    Database connection and queries realization

--- db/database.py
+++ db/database.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+
 class Connection:
     def __init__(self):
         self.connection = psycopg2.connect(
@@ -9,3 +10,20 @@ class Connection:
             port="5432",
             database="postgres"
         )
+
+    def get_data(self, _query):
+        cursor = self.connection.cursor()
+        cursor.execute(_query)
+        return cursor.fetchall()
+
+    def get_col_names(self, _query):
+        cursor = self.connection.cursor()
+        cursor.execute(_query)
+
+    def set_data(self, _query):
+        cursor = self.connection.cursor()
+        cursor.execute(_query)
+        self.connection.commit()
+
+    def close_con(self):
+        self.connection.close()
